6dd1857704c2297641d6ca623c10feb32d399b5e
Shaun Bens <bensshaun@gmail.com>
2018-01-08 22:29:22 +0000

    heroku deployment test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mongoose = require("mongoose");
 
 var PORT = process.env.PORT || 8080;
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_2x3bc8b5:phjkvp1ic9voibo190ghadl1jg@ds237967.mlab.com:37967/heroku_2x3bc8b5";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/disney-scraper";
 
 // var db = require("./models");
 
